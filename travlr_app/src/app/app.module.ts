3d1cf02b35390f32d5ecf4cb08f67f9baa390dcb
Brandon Stultz <brandon.stultz@snhu.edu>
2022-07-17 17:38:27 -0700

    completed home_page rendering

--- travlr_app/src/app/app.module.ts
+++ travlr_app/src/app/app.module.ts
@@ -1,22 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { AdboxComponent } from './components/adbox/adbox.component';
+import { ListingComponent } from './components/listing/listing.component';
+
+//import { TripDataService } from './services/trip-data.service';
+import { BlogCardComponent } from './components/blog-card/blog-card.component';
+import { TestimonialCardComponent } from './components/testimonial-card/testimonial-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomePageComponent,
+    AdboxComponent,
+    ListingComponent,
+    BlogCardComponent,
+    TestimonialCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    //TripDataService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
