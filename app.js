d4001cd7d2bacf21ec65e7d9bbbb5d1538e43f95
Brandon Stultz <brandon.stultz@snhu.edu>
2022-07-22 09:34:03 -0700

    commit

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname + '/dist')));
 
     app.get('/*', (req, res) => {
-        res.sendFile(path.join(__dirname + '/dist/index.html'))
+        res.sendFile(path.join(__dirname + '/dist/travlr-app/index.html'))
     })
 }
 
