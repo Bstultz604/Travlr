c00d3e80710e1dc9476fb97ece812eeee13cc6e4
Brandon Stultz <brandon.stultz@snhu.edu>
2022-07-22 09:15:55 -0700

    commit

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ app.use(cookieParser());
 
 //Serves built app to Heroku Server
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, 'travlr_app')));
+    app.use(express.static(path.join(__dirname + '/dist')));
 
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, 'travlr_app', 'dist', 'travlr-app','index.html'))
+    app.get('/*', (req, res) => {
+        res.sendFile(path.join(__dirname + '/dist/index.html'))
     })
 }
 
