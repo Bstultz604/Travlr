65b301c2cefb932e62106684c5be5f1c8d12ff40
Brandon Stultz <brandon.stultz@snhu.edu>
2022-07-21 19:21:39 -0700

    Fix

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.use(cookieParser());
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, '/travlr_app/build')));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html');
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     })
 }
 
